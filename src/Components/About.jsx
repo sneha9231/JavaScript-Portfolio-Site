b7f3621a43b553ae7598adc74df8590e7ef7e269
Sneha Pandey <gd6601692@gmail.com>
2024-01-21 02:06:54 +0530

    added pics

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,7 +16,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/naru.jpg";
+import image from "../images/itachi.jpg";
 
 const imageAltText = "naruto in rain";
 

bcb0f666cb40a02aff94aa82aaaf20f445074612
Sneha Pandey <gd6601692@gmail.com>
2024-01-21 02:27:57 +0530

    chg

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,7 +16,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/hokage.jpg";
+import image from "../images/socials/hokage.jpg";
 
 const imageAltText = "accomplish your dreams";
 
